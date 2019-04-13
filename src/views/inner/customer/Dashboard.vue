4363ac728be9ba1f2c0ceb60c431843050b9e5f7
voronindev <voronindev@gmail.com>
2019-04-14 02:43:26 +0300

    added slot for empty tasks

--- src/views/inner/customer/Dashboard.vue
+++ src/views/inner/customer/Dashboard.vue
@@ -55,36 +55,48 @@
     <hr>
     <p class="h5">Current tasks</p>
 
-    <div v-for="item in tasksProgress"
-         :key="item.name" class="shadow bg-light rounded">
-      <b-card
-        no-body
-        class="my-3"
-      >
-        <b-card-body>
-          <b-row no-gutters>
-            <b-col md="4">
-              <b-card-img :src="item.thumbnail" class="rounded-1"></b-card-img>
-            </b-col>
-            <b-col md="3" class="p-1 ml-2">
-              <b-card-title>
-                {{item.name}}
-              </b-card-title>
-              <b-card-sub-title class="mb-2">
-                Estimate: 2h 30min
-              </b-card-sub-title>
-              <b-card-text>
-                {{item.description}}
-              </b-card-text>
-            </b-col>
-          </b-row>
-        </b-card-body>
+    <template v-if="tasksProgress.length > 0">
+      <div
+        v-for="item in tasksProgress"
+        :key="item.name" class="shadow bg-light rounded">
+        <b-card
+          no-body
+          class="my-3"
+        >
+          <b-card-body>
+            <b-row no-gutters>
+              <b-col md="4">
+                <b-card-img :src="item.thumbnail" class="rounded-1"></b-card-img>
+              </b-col>
+              <b-col md="3" class="p-1 ml-2">
+                <b-card-title>
+                  {{item.name}}
+                </b-card-title>
+                <b-card-sub-title class="mb-2">
+                  Estimate: 2h 30min
+                </b-card-sub-title>
+                <b-card-text>
+                  {{item.description}}
+                </b-card-text>
+              </b-col>
+            </b-row>
+          </b-card-body>
 
-        <b-card-footer>
-          <b-button @click="completeTask(item.name)" variant="success" size="sm">Complete</b-button>
-        </b-card-footer>
-      </b-card>
-    </div>
+          <b-card-footer>
+            <b-button @click="completeTask(item.name)" variant="success" size="sm">Complete</b-button>
+          </b-card-footer>
+        </b-card>
+      </div>
+    </template>
+    <template v-else>
+      <div class="shadow-sm bg-light rounded">
+        <b-card no-body class="text-center">
+          <div class="text-black-50 p-3">
+            There are no tasks yet
+          </div>
+        </b-card>
+      </div>
+    </template>
 
   </div>
 </template>
