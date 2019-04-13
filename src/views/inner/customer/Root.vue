e6cf9d5544226a901650a834b22b2ff8a29f272e
roshett <roshette@yandex.ru>
2019-04-13 21:44:42 +0200

    Tasks for customer

--- src/views/inner/customer/Root.vue
+++ src/views/inner/customer/Root.vue
@@ -1,23 +1,6 @@
 <template>
   <div class="container-fluid h-100">
     <div class="row h-100">
-      <!-- <ui-sidemenu color="rgb(37, 117, 252)">
-        <div slot="items">
-          <li class="nav-item">
-            <a class="nav-link active" href="#">
-              Dashboard <span class="sr-only">(current)</span>
-            </a>
-          </li>
-        </div>
-      </ui-sidemenu>
-      <div class="content col-lg-10">
-        <transition
-          name="fade"
-          mode="out-in"
-        >
-          <router-view/>
-        </transition>
-      </div> -->
       <ui-sidemenu
         color="rgb(37, 117, 252)"
         :items="items"
