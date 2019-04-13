1baa57545728afb7139672da1a5dd640c366a3ea
voronindev <voronindev@gmail.com>
2019-04-13 18:44:07 +0300

    fixed card styling in the organisation dashboard menu

--- src/views/inner/org/Dashboard.vue
+++ src/views/inner/org/Dashboard.vue
@@ -4,35 +4,64 @@
     <hr>
     <p class="h5">Overview</p>
     <b-card-group deck>
-      <b-card title="In Progress" img-top>
+      <b-card
+        header="Open"
+        border-variant="default"
+        header-border-variant="default"
+      >
         <b-card-text>
-          <h2 class="text-primary font-weight-bold">
+          <h2 class="font-weight-bold">
+            {{tasks.open}}
+          </h2>
+        </b-card-text>
+      </b-card>
+
+      <b-card
+        header="In Progress"
+        border-variant="primary"
+        header-border-variant="primary"
+      >
+        <b-card-text>
+          <h2 class="font-weight-bold">
             {{tasks.progress}}
           </h2>
         </b-card-text>
       </b-card>
 
-      <b-card title="Complete" img-top>
+      <b-card
+        header="Complete"
+        border-variant="success"
+        header-border-variant="success"
+      >
         <b-card-text>
-          <h2 class="text-success font-weight-bold">
+          <h2 class="font-weight-bold">
             {{tasks.complete}}
           </h2>
         </b-card-text>
       </b-card>
 
-      <b-card title="Rejected" img-top>
+      <b-card
+        header="Rejected"
+        border-variant="warning"
+        header-border-variant="warning"
+      >
         <b-card-text>
-          <h2 class="text-warning font-weight-bold">
+          <h2 class="font-weight-bold">
             {{tasks.rejected}}
           </h2>
         </b-card-text>
       </b-card>
     </b-card-group>
-    <hr>
-    <p class="h5">Current state</p>
-    <b-progress height="2rem" class="mt-2" :max="1" show-value>
+    <b-progress height="2rem" class="mt-4" :max="1" show-value>
       <b-progress-bar
-        show-progress animated
+        show-progress
+        :value="(tasks.open / tasks.total)"
+        variant="secondary">
+        {{tasks.progress}}
+      </b-progress-bar>
+      <b-progress-bar
+        show-progress
+        animated
         :value="(tasks.progress / tasks.total)"
         variant="primary">
         {{tasks.progress}}
@@ -56,10 +85,11 @@ export default {
   data () {
     return {
       tasks: {
+        open: 8,
         progress: 12,
         complete: 17,
         rejected: 3,
-        total: 32
+        total: 40
       }
     }
   }
