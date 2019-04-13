0648fa9d2684c3fcc8b7eb581a30cc299335441c
roshett <roshette@yandex.ru>
2019-04-13 13:02:04 +0200

    Registration form

---
+++ src/components/RegisterFormParticipant.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          required
+          placeholder="Enter email"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Your password:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.password"
+          required
+          placeholder="Enter password"
+          type="password"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+          email: '',
+          password: '',
+        },
+        show: true
+      }
+    },
+    methods: {
+      onSubmit(evt) {
+        evt.preventDefault()
+        alert(JSON.stringify(this.form))
+      },
+      onReset(evt) {
+        evt.preventDefault()
+        // Reset our form values
+        this.form.email = ''
+        this.form.name = ''
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      }
+    }
+  }
+</script>
+
+
+
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="container">
-    <!-- Content here -->
+      <div class="row">
+        <div class="col-4"></div>
+        <div class="col-4">
+          <div>
+            <b-card no-body>
+              <b-tabs card>
+                <b-tab title="Participant" active>
+                  <b-card-text>
+                    <h3>Participant</h3 >
+                    <register-form-participant></register-form-participant>
+                  </b-card-text>
+                </b-tab>
+                <b-tab title="Fund">
+                  <b-card-text>
+                    <h3>Fund</h3 >
+                    <register-form-fund></register-form-fund>
+                  </b-card-text>
+                </b-tab>
+              </b-tabs>
+            </b-card>
+          </div>
+        </div>
+        <div class="col-4"></div>
+      </div>
   </div>
 </template>
 
@@ -11,4 +34,24 @@ export default {
 
 <style scoped>
 
+</style>
+
+<script>
+import RegisterFormParticipant from '@/components/RegisterFormParticipant.vue'
+import RegisterFormFund from '@/components/RegisterFormFund.vue'
+
+export default {
+
+  name: 'Register',
+  components: {
+    RegisterFormParticipant,
+    RegisterFormFund,
+  }
+}
+</script>
+
+<style lang="scss">
+  .nav-tabs .nav-item {
+    width: 50% !important;
+  }
 </style>
